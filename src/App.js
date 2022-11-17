771223e944001d0e1ceb8983acf6ad98e8f5215c
AnBro7 <an.brossette@gmail.com>
2022-11-17 17:48:05 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+import * as DOMPurify from "dompurify";
+import { marked } from "marked";
 
-function App() {
+export default function App() {
+  const textAreaEl = useRef(null);
+  const mdPreview = useRef(null);
+  const [textAreaValue, setTextAreaValue] = useState("");
+  const [convertedArea, setConvertedArea] = useState("");
+
+  const appContainer = {
+    display: "flex",
+    flexDirection: "column",
+  };
+
+  const sConverterContainer = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    width: "100%",
+  };
+
+  const sTextAreaContainer = {
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    padding: ".5rem",
+  };
+
+  const sTextArea = {
+    resize: "none",
+    outline: "none",
+    border: ".1rem solid",
+    borderRadius: ".5rem",
+    padding: "0.3rem 0.6rem",
+  };
+
+  useEffect(() => {
+    setConvertedArea(marked.parse(textAreaValue));
+    let cleanConvertedArea = DOMPurify.sanitize(convertedArea);
+    mdPreview.current.innerHTML = cleanConvertedArea;
+    // mdPreview.current.innerHTML = convertedArea;
+    return () => {};
+  }, [textAreaValue]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={appContainer}>
+      <div style={sConverterContainer}>
+        <div style={sTextAreaContainer}>
+          <label htmlFor="mdField">C'est ici le MD:</label>
+          <textarea
+            id="mdField"
+            name="mdField"
+            rows="20"
+            wrap="off"
+            ref={textAreaEl}
+            onChange={() => {
+              setTextAreaValue(textAreaEl.current.value);
+            }}
+            style={sTextArea}
+          />
+        </div>
+        <div style={sTextAreaContainer}>
+          <label htmlFor="mdField">La conversion:</label>
+          <textarea
+            id="mdField"
+            name="mdField"
+            rows="20"
+            wrap="off"
+            value={convertedArea}
+            readOnly
+            // ref={textAreaEl}
+            // onChange={() => {
+            //   setTextAreaValue(textAreaEl.current.value);
+            // }}
+            style={sTextArea}
+          />
+        </div>
+      </div>
+      <div ref={mdPreview}></div>
     </div>
   );
 }
-
-export default App;
